0058adeea4335053ad10543c9733134cbf5a097a
Senkara <152646045+Senkara@users.noreply.github.com>
2025-04-10 14:50:08 +0300

    Create manipulate.py

---
+++ string_package/manipulate.py
@@ -0,0 +1,17 @@
+
+
+import string
+
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_words(s):
+    return ' '.join(word.capitalize() for word in s.split())
+
+def remove_punctuation(s):
+    return s.translate(str.maketrans('', '', string.punctuation))
+
+if __name__ == "__main__":
+    print(reverse_string("hello world"))
+    print(capitalize_words("hello world"))
+    print(remove_punctuation("hello, world!"))
